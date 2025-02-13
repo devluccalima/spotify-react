bbd84f994eb7c7fa1638fd2ebf2daed84900d01a
Lucca Lima <dev.luccalima@gmail.com>
2025-02-13 17:51:18 -0300

    Migrando para o React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './componentes/header/Header';
+import Sidebar from './componentes/sidebar/Siderbar';
+import Footer from './componentes/footer/Footer';
+
+function App() {
+  return(
+    <div>
+      <Header/>
+      <Sidebar/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
